527a39cc0df7053ded272f51d0e370713623d6a9
Chandan06-08 <chandan71320050@gmail.com>
2026-05-01 23:10:12 +0530

    Optimize Procfile and database config for Railway stability

--- ethara/settings.py
+++ ethara/settings.py
@@ -61,9 +61,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ethara.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.getenv('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
         conn_max_age=600,
+        conn_health_checks=True,
     )
 }
 
